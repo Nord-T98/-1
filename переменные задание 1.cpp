6fda71b357e82d7e9f5aefacbfb165716cb51de8
Nord-T98 <aifon1998@mail.ru>
2025-03-15 12:09:47 +0300

    Add files via upload

---
+++ переменные задание 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int num;
+
+	std::cout << "Введите число:";
+	std::cin >> num;
+	std::cout << "Вы ввели:" << num << std::endl;
+	
+	std::cin.clear();
+	std::cin.ignore(32767, '\n');
+	std::cin.get();
+	return 0;
+}
