3693c68e88cdbb5bccdf899c935b19d46715d75b
Nord-T98 <aifon1998@mail.ru>
2025-03-22 00:21:33 +0300

    Update Задание 1. операторы ветвления.cpp

--- Задание 1. операторы ветвления.cpp
+++ Задание 1. операторы ветвления.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<string>
 #include<Windows.h>
 using namespace std;
 
@@ -8,9 +7,42 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	std::string result = a >= 0 ? "positive" : "negative";
-	std::cout << result << std::endl;
+	bool a = true;
+	bool b = true;
+	cout << "Оператор: ||" << endl;
+	cout << " " << endl;
+	cout << boolalpha << a << "  || " << b << " = " << (a || b) << endl;
 
-	return 0;
+	bool q = false;
+	bool w = true;
+	cout << boolalpha << q << " || " << w << " = " << (q || w) << endl;
 
-} 
+	bool e = true;
+	bool r = false;
+	cout << boolalpha << e << "  || " << r << " = " << (e || r) << endl;
+
+	bool t = false;
+	bool y = false;
+	cout << boolalpha << t << " || " << y << " = " << (t || y) << endl;
+	cout << " " << endl;
+
+	cout << "Оператор: &&" << endl;
+
+	bool f = true;
+	bool g = true;
+	cout << boolalpha << f << "  && " << g << " = " << (f && g) << endl;
+
+	bool h = false;
+	bool j = true;
+	cout << boolalpha << h << " && " << j << " = " << (h && j) << endl;
+
+	bool k = true;
+	bool l = false;
+	cout << boolalpha << k << "  && " << l << " = " << (k && l) << endl;
+
+	bool n = false;
+	bool m = false;
+	cout << boolalpha << n << " && " << m << " = " << (n && m) << endl;
+
+    return 0;
+}
